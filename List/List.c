219999e21d6baff060fed574c89f79820ea47e0e
LegendaryHog <kuteinikov.sd@phystech.edu>
2022-03-28 12:30:12 +0300

    create List

---
+++ List/List.c
@@ -0,0 +1,43 @@
+#include "List.h"
+
+
+Node* ListCtor (void)
+{
+    return (Node*) calloc (1, sizeof (Node));
+}
+
+Node* NodeInsAft (Node* last, data_t push)
+{
+    if (last == NULL)
+    {
+        Node* new = (Node*) calloc (1, sizeof (Node));
+        new->data = push;
+        return new;
+    }
+    Node* new = (Node*) calloc (1, sizeof (Node));
+    new->next = last->next;
+    last->next = new;
+    new->data = push;
+    return new;
+}
+
+int NodeDeleteAft (Node* node)
+{
+    if (node->next == NULL)
+    {
+        return ERR;
+    }
+    Node* tmpnode = node->next;
+    node->next = node->next->next;
+    free (tmpnode);
+    return NO_ERR;
+}
+
+int ListDtor (Node* first)
+{
+    assert (first != NULL);
+    while (NodeDeleteAft (first) != ERR) {;}
+    free (first);
+    return NO_ERR;
+}
+
---
+++ List/List.h
@@ -0,0 +1,21 @@
+#ifndef LISTH
+#define LISTH
+
+#include "../common.h" 
+
+enum ERRORS {
+    NO_ERR = 0,
+    ERR = 1,
+};
+typedef struct Node {
+    data_t data;
+    struct Node* next;
+} Node;
+
+Node* ListCtor      (void);
+int   ListDtor      (Node* node);
+Node* NodeInsAft    (Node* last, data_t push);
+int   NodeDeleteAft (Node* elem);
+int   ListDump      (Node* node);
+
+#endif
